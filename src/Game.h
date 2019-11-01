917491cd860ec0cd99ff2f41f6b2b9b1f02108ed
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2019-11-01 14:44:16 +0100

    Create Game class

---
+++ src/Game.h
@@ -0,0 +1,11 @@
+#ifndef GOLD_HACK_GAME_H
+#define GOLD_HACK_GAME_H
+
+#include <vector>
+#include "Level.h"
+
+class Game {
+    std::vector<Level<10, 10>> levels;
+};
+
+#endif //GOLD_HACK_LEVEL_H
