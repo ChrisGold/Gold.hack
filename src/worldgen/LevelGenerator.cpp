4e86861efb85ab9d306e90f846a455b3ca78cfac
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2019-11-18 13:05:31 +0100

    Fix segfault

--- src/worldgen/LevelGenerator.cpp
+++ src/worldgen/LevelGenerator.cpp
@@ -1,11 +1,11 @@
 #include "LevelGenerator.h"
 
 void LevelGenerator::room(sf::IntRect rect, int floor_material, int wall_material) {
-    for (int x = rect.left; x < rect.left + rect.width; ++x) {
-        for (int y = rect.top; x < rect.top + rect.height; ++y) {
-            LevelTile &t = this->level[x][y];
-            t.pass_north = y == rect.top;
-            t.pass_east = x == rect.top + rect.height - 1;
+    for (int x = 0; x < rect.width; ++x) {
+        for (int y = 0; y < rect.height; ++y) {
+            LevelTile &t = this->level[rect.left + x][rect.top + y];
+            t.pass_north = y != 0;
+            t.pass_east = x != rect.width - 1;
             t.floor_tile = floor_material;
             t.wall_tile = wall_material;
         }
@@ -23,7 +23,7 @@ void LevelGenerator::generate() {
 LevelGenerator::LevelGenerator() {
     for (int x = 0; x < LEVEL_X_SIZE; x++) {
         for (int y = 0; y < LEVEL_Y_SIZE; y++) {
-            LevelTile t = level[x][y];
+            LevelTile &t = level[x][y];
             t.pass_east = false;
             t.pass_north = false;
             t.floor_tile = 0;
