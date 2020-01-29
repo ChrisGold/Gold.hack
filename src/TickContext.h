4ed7415dfc7d75bed34c3c4e285b446e23afbf6d
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2020-01-29 10:31:38 +0100

    Document TickContext.h

--- src/TickContext.h
+++ src/TickContext.h
@@ -7,13 +7,22 @@ class Game;
 
 class Level;
 
+/**
+ * An encapsulated context of a single Tick in the game.
+ */
 struct TickContext {
     Game *game;
     Level *level;
     Inventory *playerInventory;
+    /**
+     * A running counter of ticks, unique to each game.
+     */
     const long tick_nr;
     const time_t wall_time;
     float difficulty;
+    /**
+     * A pointer to the messageQueue displayed in the inventory.
+     */
     std::vector<std::string> *messageQueue;
 
     TickContext(Game *game, Level *level, Inventory *playerInventory, const long tickNr, const time_t wallTime,
