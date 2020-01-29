5db230153595e09316e84eadfaa0e7264674a87d
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2020-01-29 10:27:59 +0100

    Create documentation for main.cpp and Game.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include "src/Game.h"
 
+/**
+ * The main function. It takes no arguments.
+ * @return 0 for normal termination, non-zero for anomalous termination.
+ */
 int main()
 {
     auto game = std::make_shared<Game>();
--- src/Game.h
+++ src/Game.h
@@ -12,16 +12,33 @@
 #include "GameStage.h"
 #include "resources/Resources.h"
 
+/**
+ * The main Game object. This keeps track of all objects that can only exist centrally.
+ */
 class Game {
     std::vector<Level> levels;
     Menu menu;
 
+    /**
+     * The main window.
+     */
     sf::RenderWindow window;
 
+    /**
+     * The sprite representing the level, later drawn into the main window.
+     */
     sf::Sprite levelSprite;
+    /**
+     * The sf::RenderTexture into which the level is drawn.
+     */
     sf::RenderTexture levelTexture;
-
+    /**
+     * The sprite representing the sidebar with the player inventory, later drawn into the main window.
+     */
     sf::Sprite sidebarSprite;
+    /**
+    * The sf::RenderTexture into which the inventory sidebar is drawn.
+    */
     sf::RenderTexture sidebarTexture;
 
 
